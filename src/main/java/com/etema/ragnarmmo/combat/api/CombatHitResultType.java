5e3f065a9fe32faadff081d0240d837bd12014b6
Etema <etemalaxe@gmail.com>
2026-04-05 03:30:29 -0400

    combat: add combat hit result enum

---
+++ src/main/java/com/etema/ragnarmmo/combat/api/CombatHitResultType.java
@@ -0,0 +1,14 @@
+package com.etema.ragnarmmo.combat.api;
+
+/**
+ * Explicit combat resolution outcome types.
+ */
+public enum CombatHitResultType {
+    HIT,
+    CRIT,
+    MISS,
+    DODGE,
+    BLOCKED,
+    IMMUNE,
+    HEAL
+}
